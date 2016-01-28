3c38ac39c36aba9c579bedf1fcfa07981dd7d1f1
strategy155 <strategy155@gmail.com>
2016-01-28 16:24:35 +0300

    DownloadFileFunction added but not organized

--- processingScript.py
+++ processingScript.py
@@ -1,2 +1,4 @@
-import os
-import marshal
+import urllib.request
+
+urllib.request.urlretrieve("http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-ki.gz", "kiNgramRussianGz")
+print('r')
