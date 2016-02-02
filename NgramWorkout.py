842f053278a9be0416ad1b9b810b8e9df8f24fc2
strategy155 <strategy155@gmail.com>
2016-02-02 10:22:57 +0300

    Edited some commentary

--- NgramWorkout.py
+++ NgramWorkout.py
@@ -1,6 +1,5 @@
 import RussianSnowballStemmer
 import urllib.request
-import io
 import gzip
 
 
@@ -17,14 +16,14 @@ file = download_and_save_file_locally(
 file2 = download_and_save_file_locally(
     'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-br.gz', 2)
 
-stemmer = RussianSnowballStemmer.Stemmer() #just to ease codewriting
+stemmer = RussianSnowballStemmer.Stemmer() # just to ease coding
 
 
 for line in file:
     line = line.rstrip('\n')
     fields = line.split('\t')
-    fields[0] = fields[0].split(' ') #splitted words
-    fields[0][0]= fields[0][0].split('_')[0] #removed _VERB tag
-    if "кида" == stemmer.stem(fields[0][0]) and "_NOUN" in fields[0][1]: #checking if second word is NOUN
+    fields[0] = fields[0].split(' ') # splitted words
+    fields[0][0]= fields[0][0].split('_')[0] # removed _VERB tag
+    if "кида" == stemmer.stem(fields[0][0]) and "_NOUN" in fields[0][1]: # checking if second word is NOUN
         print(fields)
-        fields[0][1]= fields[0][1].split('_')[0] #removed _NOUN tag
+        fields[0][1]= fields[0][1].split('_')[0] # removed _NOUN tag
