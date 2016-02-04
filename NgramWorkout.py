d9a2fde10e5aa2521227a38970f2cd2dc51f8287
strategy155 <strategy155@gmail.com>
2016-02-04 16:33:57 +0300

    Finally setted the algorithm up, added pymorph form analysis

--- NgramWorkout.py
+++ NgramWorkout.py
@@ -1,29 +1,45 @@
 import RussianSnowballStemmer
 import urllib.request
+import urllib.error
 import gzip
+import pip
+
+pip.main(['install','--upgrade','pymorphy2'])
 
 
-def download_and_save_file_locally(file_url, counter=1):
-    file_name = "ngram" + str(counter)
+def download_and_save_file_locally(file_url):
+    file_name =file_url.split('/')[-1]
     try:
         return gzip.open(file_name, 'rt', encoding='utf-8')
     except IOError:
-        urllib.request.urlretrieve(file_url, file_name)
-        return gzip.open(file_name, 'rt', encoding='utf-8')
+        try:
+            urllib.request.urlretrieve(file_url, file_name)
+            return gzip.open(file_name, 'rt', encoding='utf-8')
+        except urllib.error.URLError:
+            print("Internet connection is down, try later")
+            exit()
 
 file = download_and_save_file_locally(
-    'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-ki.gz', 1)
+    'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-ki.gz')
 file2 = download_and_save_file_locally(
-    'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-br.gz', 2)
+    'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-br.gz')
 
 stemmer = RussianSnowballStemmer.Stemmer() # just to ease coding
+
+dictionaryOfObjects = {}
 
 
 for line in file:
     line = line.rstrip('\n')
-    fields = line.split('\t')
-    fields[0] = fields[0].split(' ') # splitted words
-    fields[0][0]= fields[0][0].split('_')[0] # removed _VERB tag
-    if "кида" == stemmer.stem(fields[0][0]) and "_NOUN" in fields[0][1]: # checking if second word is NOUN
-        print(fields)
-        fields[0][1]= fields[0][1].split('_')[0] # removed _NOUN tag
+    ngramFields = line.split('\t')
+    ngramFields[0] = ngramFields[0].split(' ') # splitted words
+    ngramFields[0][0]= ngramFields[0][0].split('_')[0] # removed _VERB tag
+    if "кида" == stemmer.stem(ngramFields[0][0]) and "_NOUN" in ngramFields[0][1]: # checking if second word is NOUN
+        # print(ngramFields)
+        ngramFields[0][1]= ngramFields[0][1].split('_')[0] # removed _NOUN tag
+        # print(ngramFields[0][1])
+        try:
+            dictionaryOfObjects[ngramFields[0][1]] += ngramFields[2]
+        except KeyError:
+            dictionaryOfObjects[ngramFields[0][1]] = ngramFields[2]
+print(dictionaryOfObjects)
