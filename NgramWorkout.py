a65bff94a150b2895e4e99f47807e5663e1fb639
strategy155 <strategy155@gmail.com>
2016-02-05 12:45:13 +0300

    Organised functions

--- NgramWorkout.py
+++ NgramWorkout.py
@@ -1,27 +1,15 @@
 import RussianSnowballStemmer
-import urllib.request
-import urllib.error
-import gzip
-import pip
+import UsefulFunctions
 
-pip.main(['install','--upgrade','pymorphy2'])
+try:
+    import pymorphy2
+except ImportError:
+    UsefulFunctions.install_and_import('pymorphy2')
 
 
-def download_and_save_file_locally(file_url):
-    file_name =file_url.split('/')[-1]
-    try:
-        return gzip.open(file_name, 'rt', encoding='utf-8')
-    except IOError:
-        try:
-            urllib.request.urlretrieve(file_url, file_name)
-            return gzip.open(file_name, 'rt', encoding='utf-8')
-        except urllib.error.URLError:
-            print("Internet connection is down, try later")
-            exit()
-
-file = download_and_save_file_locally(
+file = UsefulFunctions.download_and_open_gzip(
     'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-ki.gz')
-file2 = download_and_save_file_locally(
+file2 = UsefulFunctions.download_and_open_gzip(
     'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-br.gz')
 
 stemmer = RussianSnowballStemmer.Stemmer() # just to ease coding
@@ -29,17 +17,18 @@ stemmer = RussianSnowballStemmer.Stemmer() # just to ease coding
 dictionaryOfObjects = {}
 
 
-for line in file:
-    line = line.rstrip('\n')
-    ngramFields = line.split('\t')
-    ngramFields[0] = ngramFields[0].split(' ') # splitted words
-    ngramFields[0][0]= ngramFields[0][0].split('_')[0] # removed _VERB tag
-    if "кида" == stemmer.stem(ngramFields[0][0]) and "_NOUN" in ngramFields[0][1]: # checking if second word is NOUN
-        # print(ngramFields)
-        ngramFields[0][1]= ngramFields[0][1].split('_')[0] # removed _NOUN tag
-        # print(ngramFields[0][1])
-        try:
-            dictionaryOfObjects[ngramFields[0][1]] += ngramFields[2]
-        except KeyError:
-            dictionaryOfObjects[ngramFields[0][1]] = ngramFields[2]
-print(dictionaryOfObjects)
+#
+# for line in file:
+#     line = line.rstrip('\n')
+#     ngramFields = line.split('\t')
+#     ngramFields[0] = ngramFields[0].split(' ') # splitted words
+#     ngramFields[0][0]= ngramFields[0][0].split('_')[0] # removed _VERB tag
+#     if "кида" == stemmer.stem(ngramFields[0][0]) and "_NOUN" in ngramFields[0][1]: # checking if second word is NOUN
+#         # print(ngramFields)
+#         ngramFields[0][1]= ngramFields[0][1].split('_')[0] # removed _NOUN tag
+#         # print(ngramFields[0][1])
+#         try:
+#             dictionaryOfObjects[ngramFields[0][1]] += ngramFields[2]
+#         except KeyError:
+#             dictionaryOfObjects[ngramFields[0][1]] = ngramFields[2]
+# print(dictionaryOfObjects)
