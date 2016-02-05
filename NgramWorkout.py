fdbbb7aff285462ddeec57c0649ca8a0e71ef2dc
strategy155 <strategy155@gmail.com>
2016-02-05 23:05:50 +0300

    Finished alghorithm

--- NgramWorkout.py
+++ NgramWorkout.py
@@ -1,10 +1,14 @@
 import RussianSnowballStemmer
 import UsefulFunctions
+import sys
+import time
 
 try:
-    import pymorphy2
+        import pymorphy2
 except ImportError:
-    UsefulFunctions.install_and_import('pymorphy2')
+    UsefulFunctions.install('pymorphy2')
+    UsefulFunctions.install('pymorphy2-dicts-ru')
+    import pymorphy2
 
 
 file = UsefulFunctions.download_and_open_gzip(
@@ -12,23 +16,31 @@ file = UsefulFunctions.download_and_open_gzip(
 file2 = UsefulFunctions.download_and_open_gzip(
     'http://storage.googleapis.com/books/ngrams/books/googlebooks-rus-all-2gram-20120701-br.gz')
 
-stemmer = RussianSnowballStemmer.Stemmer() # just to ease coding
-
+morph = pymorphy2.MorphAnalyzer()
 dictionaryOfObjects = {}
-
-
-#
-# for line in file:
-#     line = line.rstrip('\n')
-#     ngramFields = line.split('\t')
-#     ngramFields[0] = ngramFields[0].split(' ') # splitted words
-#     ngramFields[0][0]= ngramFields[0][0].split('_')[0] # removed _VERB tag
-#     if "кида" == stemmer.stem(ngramFields[0][0]) and "_NOUN" in ngramFields[0][1]: # checking if second word is NOUN
-#         # print(ngramFields)
-#         ngramFields[0][1]= ngramFields[0][1].split('_')[0] # removed _NOUN tag
-#         # print(ngramFields[0][1])
-#         try:
-#             dictionaryOfObjects[ngramFields[0][1]] += ngramFields[2]
-#         except KeyError:
-#             dictionaryOfObjects[ngramFields[0][1]] = ngramFields[2]
-# print(dictionaryOfObjects)
+counter = 0
+counter1 = 0
+for line in file:
+    counter +=1
+    line = line.rstrip('\n')
+    ngramFields = line.split('\t')
+    ngramFields[0] = ngramFields[0].split(' ') # splitted words
+    ngramFields[0][0]= ngramFields[0][0].split('_')[0] # removed tag
+    ngramFields[0][1]= ngramFields[0][1].split('_')[0] # removed tag
+    firstParsedWord = morph.parse(ngramFields[0][0])[0]
+    secondParsedWord = morph.parse(ngramFields[0][1])[0]
+    if 'кидать' == firstParsedWord.normal_form and ({'NOUN', 'accs'} in secondParsedWord.tag or
+                     {'NPRO', 'accs'} in secondParsedWord.tag):  # проверка на винительный падеж(прямой объект)
+        # print(ngramFields)
+        # print(ngramFields[0][1])
+        counter1 +=1
+        try:
+            dictionaryOfObjects[ngramFields[0][1]] += int(ngramFields[2])
+        except KeyError:
+            dictionaryOfObjects[ngramFields[0][1]] = int(ngramFields[2])
+    sys.stdout.write('\r')
+    sys.stdout.write("Parsing progress: " + str(counter) + "  lines parsed |" + "Analysis progress: " + str(counter1) + " вхождения кидать " )
+    sys.stdout.flush()
+print(dictionaryOfObjects)
+file.close()
+file2.close()
