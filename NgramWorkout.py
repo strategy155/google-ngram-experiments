4ba546dd95edcd2e6e26b3b1bb777fc58bdd0851
strategy155 <strategy155@gmail.com>
2016-02-06 10:20:25 +0300

    Final commit @

--- NgramWorkout.py
+++ NgramWorkout.py
@@ -1,7 +1,6 @@
-import RussianSnowballStemmer
 import UsefulFunctions
 import sys
-import time
+
 
 try:
         import pymorphy2
@@ -18,6 +17,7 @@ file2 = UsefulFunctions.download_and_open_gzip(
 
 morph = pymorphy2.MorphAnalyzer()
 dictionaryOfObjects = {}
+dictionaryOfObjects1 = {}
 counter = 0
 counter1 = 0
 for line in file:
@@ -29,8 +29,8 @@ for line in file:
     ngramFields[0][1]= ngramFields[0][1].split('_')[0] # removed tag
     firstParsedWord = morph.parse(ngramFields[0][0])[0]
     secondParsedWord = morph.parse(ngramFields[0][1])[0]
-    if 'кидать' == firstParsedWord.normal_form and ({'NOUN', 'accs'} in secondParsedWord.tag or
-                     {'NPRO', 'accs'} in secondParsedWord.tag):  # проверка на винительный падеж(прямой объект)
+    if 'кидать' == firstParsedWord.normal_form and ({'NOUN', 'accs'} in secondParsedWord.tag or {'NOUN', 'nomn'} in secondParsedWord.tag or
+                     {'NPRO', 'accs'} in secondParsedWord.tag or {'NOUN','ablt'} in secondParsedWord.tag):  # проверка на винительный падеж(прямой объект)
         # print(ngramFields)
         # print(ngramFields[0][1])
         counter1 +=1
@@ -39,8 +39,34 @@ for line in file:
         except KeyError:
             dictionaryOfObjects[ngramFields[0][1]] = int(ngramFields[2])
     sys.stdout.write('\r')
-    sys.stdout.write("Parsing progress: " + str(counter) + "  lines parsed |" + "Analysis progress: " + str(counter1) + " вхождения кидать " )
+    sys.stdout.write("Parsing progress: " + str(counter) + "  lines parsed |" + "Analysis progress: " + str(counter1) + " count of  'кидать' " )
     sys.stdout.flush()
+print('\n')
+counter = 0
+counter1 = 0
+for line in file2:
+    counter +=1
+    line = line.rstrip('\n')
+    ngramFields = line.split('\t')
+    ngramFields[0] = ngramFields[0].split(' ') # splitted words
+    ngramFields[0][0]= ngramFields[0][0].split('_')[0] # removed tag
+    ngramFields[0][1]= ngramFields[0][1].split('_')[0] # removed tag
+    firstParsedWord = morph.parse(ngramFields[0][0])[0]
+    secondParsedWord = morph.parse(ngramFields[0][1])[0]
+    if 'бросать' == firstParsedWord.normal_form and ({'NOUN', 'accs'} in secondParsedWord.tag or {'NOUN', 'nomn'} in secondParsedWord.tag or
+                     {'NPRO', 'accs'} in secondParsedWord.tag or {'NOUN','ablt'} in secondParsedWord.tag):  # проверка на винительный падеж(прямой объект)
+        # print(ngramFields)
+        # print(ngramFields[0][1])
+        counter1 +=1
+        try:
+            dictionaryOfObjects1[ngramFields[0][1]] += int(ngramFields[2])
+        except KeyError:
+            dictionaryOfObjects1[ngramFields[0][1]] = int(ngramFields[2])
+    sys.stdout.write('\r')
+    sys.stdout.write("Parsing progress: " + str(counter) + "  lines parsed |" + "Analysis progress: " + str(counter1) + " count of  'бросать' " )
+    sys.stdout.flush()
+
 print(dictionaryOfObjects)
+print(dictionaryOfObjects1)
 file.close()
 file2.close()
